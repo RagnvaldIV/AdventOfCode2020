32bb55de5e4488ed6be6047f303cb99b7a391e86
RagnvaldIV <nicolasbernt@gmail.com>
2020-12-16 22:14:44 +0100

    nicer output

--- Day 4/part1.py
+++ Day 4/part1.py
@@ -2,9 +2,13 @@ inputTxt = open("Day 4/input.txt", "r")
 ppCount = 0
 # ppFields = []
 ppValidFields = 0
-# binFields = 0
-
-
+binFields = 0
+# lines = 1
+# for line in inputTxt.readlines():
+#     if len(line) == 1:
+#         lines += 1
+# print(lines)
+# inputTxt.seek(0,0)
 
 def binaryRepresentation(tag, binFields):
     if tag == 'byr':
@@ -25,25 +29,26 @@ def binaryRepresentation(tag, binFields):
         binFields += 1
     return binFields
 
-for line in inputTxt:
+
+for line in inputTxt.readlines():
     if line[0] != '\n':
         # line has text, split and add to fields
         fields = line.split()
         for field in fields:
             # Convert to binary to check
-            # binFields = binaryRepresentation(field[0:3], binFields)
+            binFields = binaryRepresentation(field[0:3], binFields)
             if field[0] != 'c':
                 # ppFields.append(field)
                 ppValidFields += 1
     else:
         # Check if this last passport was valid, reset array
         if ppValidFields == 7:
-            # print(format(binFields, 'b').zfill(8))
+            print(format(binFields, 'b').zfill(8))
             ppCount += 1
-        # else:
-            # print(format(binFields, 'b').zfill(8) + "<------ INVALID")
+        else:
+            print(format(binFields, 'b').zfill(8) + "<------ INVALID")
         # ppFields.clear()
         binFields = 0
         ppValidFields = 0
 
-print(ppCount)
+print("Valid: " + str(ppCount))
