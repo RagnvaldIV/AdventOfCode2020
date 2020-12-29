e5241580e5cd9c47404a3a4c57ba7818904d3e82
RagnvaldIV <nicolasbernt@gmail.com>
2020-12-29 13:51:48 +0100

    Removed debug prints

--- Day 7/part2.py
+++ Day 7/part2.py
@@ -3,15 +3,12 @@ rules = inputTxt.readlines()
 myBag = "shiny gold"
 
 def getChildren(bag):
-    print("Getting children for " + bag)
     childrenCount = 1
     for rule in rules:
         if rule.startswith(bag):
             if rule.count("no other bag"):
-                print("No children")
                 return childrenCount
             childList = rule.split("contain ")[1].split(", ")
-            print(childList)
             for child in childList:
                 count = int(child[0])
                 child = child[2:len(child)].split(" bag")[0]
